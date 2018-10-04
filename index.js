7f5bcc449945a5eac7db93d6b9ea0077454e2428
CanhGosuu <safetycanh@gmail.com>
2018-10-05 00:01:10 +0700

    user.routes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+var db = require("./db");
 //
 var bodyParser = require("body-parser");
 // support parsing of application/json type post data
@@ -7,85 +8,21 @@ app.use(bodyParser.json());
 //support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: true }));
 //
-const low = require("lowdb");
-const FileSync = require("lowdb/adapters/FileSync");
-
-const adapter = new FileSync("db.json");
-const db = low(adapter);
-// Set some defaults (required if your JSON file is empty)
-db.defaults({ users: [] }).write();
 //
-
-var shortid = require("shortid");
-
-//
-
 const port = 3000;
 //
 app.set("view engine", "ejs");
 app.set("views", "./views");
 //
-//sẽ đổi thành ejs sau này
-//
-// var users = [
-//   { id: 1, name: "Ty" },
-//   { id: 2, name: "Suu" },
-//   { id: 3, name: "Dan" },
-//   { id: 4, name: "Mao" }
-// ];
+var userRoute = require("./routes/user.routes");
 //
 app.get("/", (req, res) => {
   res.render("index", {
     name: "Ta Cao Canh"
   });
 });
-app.get("/users", (req, res) => {
-  res.render("users/index", {
-    users: db.get("users").value()
-  });
-});
-app.get("/users/search", (req, res) => {
-  var users = db.get("users").value();
-  var _name = req.query._name.toLowerCase();
-  var mathchUsers = users.filter(user => {
-    var userName = user.name.toLowerCase();
-    return userName.indexOf(_name) != -1;
-    // return user.name.toLowerCase().indexOf(q.toLowerCase()) != -1;
-  });
-  res.render("users/index", {
-    users: mathchUsers
-  });
-});
-//
-app.get("/users/create", (req, res) => {
-  res.render("users/create");
-});
-app.post("/users/create", (req, res) => {
-  db.get("users")
-    .push({
-      id: shortid.generate(),
-      name: req.param("name"),
-      email: req.param("email")
-    })
-    //cách 2:
-    //req.body.id=id;
-    // .push(req.body)
-    .write();
-  // users.push(req.body); // lưu lại tất cả những gì đang được nhập trong create page
-  res.redirect("/users");
-});
-//
-app.get("/users/:id", (req, res) => {
-  //dinamic param sử dụng toán tử ":"
-  var id = req.params.id;
-  var user = db
-    .get("users")
-    .find({ id: id })
-    .value();
-  res.render("users/view", {
-    user: user
-  });
-});
+//connect rountes
+app.use("/users", userRoute);
 //
 app.listen(port, () => {
   console.log("Listen in  port " + port);
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,3 +1,60 @@
 const express = require("express");
+var db = require("../db");
+var shortid = require("shortid");
+//
+//
 const routes = express.Router();
+//
+var bodyParser = require("body-parser");
+// support parsing of application/json type post data
+routes.use(bodyParser.json());
+//support parsing of application/x-www-form-urlencoded post data
+routes.use(bodyParser.urlencoded({ extended: true }));
+
+routes.get("/", (req, res) => {
+  res.render("users/index", {
+    users: db.get("users").value()
+  });
+});
+routes.get("/search", (req, res) => {
+  var users = db.get("users").value();
+  var _name = req.query._name.toLowerCase();
+  var mathchUsers = users.filter(user => {
+    var userName = user.name.toLowerCase();
+    return userName.indexOf(_name) != -1;
+    // return user.name.toLowerCase().indexOf(q.toLowerCase()) != -1;
+  });
+  res.render("users/index", {
+    users: mathchUsers
+  });
+});
+//
+routes.get("/create", (req, res) => {
+  res.render("users/create");
+});
+routes.post("/create", (req, res) => {
+  req.body.id = shortid.generate();
+  db.get("users")
+    .push(req.body)
+    .write();
+  // cach 2:   {id: shortid.generate(),
+  //            name: req.param("name"),
+  //            email: req.param("email")}
+
+  res.redirect("/");
+});
+//
+routes.get("/:id", (req, res) => {
+  //dinamic param sử dụng toán tử ":"
+  var id = req.params.id;
+  var user = db
+    .get("users")
+    .find({ id: id })
+    .value();
+  res.render("users/view", {
+    user: user
+  });
+});
+//
+
 module.exports = routes;
