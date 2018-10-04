b65ba06d136400c5842158f36bae308606f6a1a9
CanhGosuu <safetycanh@gmail.com>
2018-10-05 00:19:59 +0700

    UserControllerr

---
+++ controller/user.controller.js
@@ -0,0 +1,52 @@
+var express = require("express");
+const routes = express.Router();
+
+var shortid = require("shortid");
+var db = require("../db");
+//
+var bodyParser = require("body-parser");
+// support parsing of application/json type post data
+routes.use(bodyParser.json());
+//support parsing of application/x-www-form-urlencoded post data
+routes.use(bodyParser.urlencoded({ extended: true }));
+//
+module.exports = {
+  listUser: (req, res) => {
+    res.render("users/index", {
+      users: db.get("users").value()
+    });
+  },
+  search: (req, res) => {
+    var users = db.get("users").value();
+    var _name = req.query._name.toLowerCase();
+    var mathchUsers = users.filter(user => {
+      var userName = user.name.toLowerCase();
+      return userName.indexOf(_name) != -1;
+      // return user.name.toLowerCase().indexOf(q.toLowerCase()) != -1;
+    });
+    res.render("users/index", {
+      users: mathchUsers
+    });
+  },
+  create: (req, res) => {
+    req.body.id = shortid.generate();
+    db.get("users")
+      .push(req.body)
+      .write();
+    // cach 2:   {id: shortid.generate(),
+    //            name: req.param("name"),
+    //            email: req.param("email")}
+
+    res.redirect("/");
+  },
+  viewUser: (req, res) => {
+    var id = req.params.id;
+    var user = db
+      .get("users")
+      .find({ id: id })
+      .value();
+    res.render("users/view", {
+      user: user
+    });
+  }
+};
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,60 +1,19 @@
 const express = require("express");
 var db = require("../db");
-var shortid = require("shortid");
+
 //
+var UserController = require("../controller/user.controller");
 //
 const routes = express.Router();
-//
-var bodyParser = require("body-parser");
-// support parsing of application/json type post data
-routes.use(bodyParser.json());
-//support parsing of application/x-www-form-urlencoded post data
-routes.use(bodyParser.urlencoded({ extended: true }));
 
-routes.get("/", (req, res) => {
-  res.render("users/index", {
-    users: db.get("users").value()
-  });
-});
-routes.get("/search", (req, res) => {
-  var users = db.get("users").value();
-  var _name = req.query._name.toLowerCase();
-  var mathchUsers = users.filter(user => {
-    var userName = user.name.toLowerCase();
-    return userName.indexOf(_name) != -1;
-    // return user.name.toLowerCase().indexOf(q.toLowerCase()) != -1;
-  });
-  res.render("users/index", {
-    users: mathchUsers
-  });
-});
+routes.get("/", UserController.listUser);
+routes.get("/search", UserController.search);
 //
 routes.get("/create", (req, res) => {
   res.render("users/create");
 });
-routes.post("/create", (req, res) => {
-  req.body.id = shortid.generate();
-  db.get("users")
-    .push(req.body)
-    .write();
-  // cach 2:   {id: shortid.generate(),
-  //            name: req.param("name"),
-  //            email: req.param("email")}
-
-  res.redirect("/");
-});
+routes.post("/create", UserController.create);
 //
-routes.get("/:id", (req, res) => {
-  //dinamic param sử dụng toán tử ":"
-  var id = req.params.id;
-  var user = db
-    .get("users")
-    .find({ id: id })
-    .value();
-  res.render("users/view", {
-    user: user
-  });
-});
+routes.get("/:id", UserController.viewUser); //dinamic param sử dụng toán tử ":"
 //
-
 module.exports = routes;
