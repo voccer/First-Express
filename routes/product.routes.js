0ed8e7843bd7524df7d3daa1ab140dd6eddf0be8
CanhGosuu <safetycanh@gmail.com>
2018-10-08 16:47:27 +0700

    quite good

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -2,13 +2,13 @@ const express = require("express");
 //
 var ProductController = require("../controller/product.controller");
 //
-// var validate = require("../validate/product.validate");
+var validate = require("../validate/product.validate");
 //
 const routes = express.Router();
 //
 routes.get("/", ProductController.listProduct);
 routes.get("/search", ProductController.searchPro);
 routes.get("/create", ProductController.GETcreatePro);
-routes.post("/create", ProductController.POSTcreatePro);
+routes.post("/create", validate.POSTcreatePro, ProductController.POSTcreatePro);
 routes.get("/:id", ProductController.getProduct); //dinamic param sử dụng toán tử ":"
 module.exports = routes;
--- validate/product.validate.js
+++ validate/product.validate.js
@@ -1,5 +1,19 @@
-// module.exports={
-//     POSTcreatePro=(req, res, next)=>{
-//         next();
-//     }
-// }
+module.exports = {
+  POSTcreatePro: (req, res, next) => {
+    let Err = [];
+    if (!req.body.namePro) {
+      Err.push("Name is required!!");
+    }
+    if (!req.body.price) {
+      Err.push("Price is required!!");
+    }
+    if (Err.length > 0) {
+      res.render("product/create", {
+        Err: Err,
+        Value: req.body
+      });
+      return;
+    }
+    next();
+  }
+};
